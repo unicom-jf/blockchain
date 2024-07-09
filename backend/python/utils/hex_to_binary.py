9be06388ab8e500803e4266e6b27cb5a2c4abcf4
unicom_jf10 <unicom_jf10@163.com>
2024-07-08 22:09:35 -0500

    patch hex_to_binary

--- backend/python/utils/hex_to_binary.py
+++ backend/python/utils/hex_to_binary.py
@@ -4,9 +4,13 @@ def hex_to_binary(hex):
     rslt += '{:0>4d}'.format(
       int(
         format(int('0x' + i, 16), 'b')
-        , 2)
+        , 10)
     )
+  return rslt
 
 def main():
   print(f"hex_to_binary('12'): {hex_to_binary('12')}")
   print(f"hex_to_binary('1e'): {hex_to_binary('1e')}")
+
+if __name__ == '__main__':
+  main()
